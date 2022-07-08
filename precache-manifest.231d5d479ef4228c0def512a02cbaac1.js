fac5be2cf79ceda9be9b3597c73ebecb17b21207
alaili02 <mohammadalayli9@gmail.com>
2022-07-08 00:14:12 -0400

    Updates

---
+++ precache-manifest.231d5d479ef4228c0def512a02cbaac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59a2eb545b8e2a924a20",
+    "url": "/static/js/main.59a2eb54.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "45c3978e9860269de2df",
+    "url": "/static/js/2.45c3978e.chunk.js"
+  },
+  {
+    "revision": "1d5addb8a819dfdd6347",
+    "url": "/static/js/runtime~main.1d5addb8.js"
+  },
+  {
+    "revision": "0d87ab6bebdbfbded8b597a7bf218bd2",
+    "url": "/index.html"
+  }
+];
