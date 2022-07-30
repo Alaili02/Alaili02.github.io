299627a930d4ad1ecc4f421a49af25b1628bfd1a
alaili02 <mohammadalayli9@gmail.com>
2022-07-30 00:11:30 -0400

    Updates

---
+++ precache-manifest.e1e64b9ac9b3de04452d1bc761a5a8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "246f7a0c0bd710e08b38",
+    "url": "/static/js/main.246f7a0c.chunk.js"
+  },
+  {
+    "revision": "269d177dfa7b1bf5e155",
+    "url": "/static/js/1.269d177d.chunk.js"
+  },
+  {
+    "revision": "a8f3d432ff95325173d3",
+    "url": "/static/js/2.a8f3d432.chunk.js"
+  },
+  {
+    "revision": "7ac455435b7f93cbf8f6",
+    "url": "/static/js/runtime~main.7ac45543.js"
+  },
+  {
+    "revision": "364af80acaf3ee30330e560e74452012",
+    "url": "/static/media/Sticker_App_Cen_01.364af80a.webp"
+  },
+  {
+    "revision": "b4249d31cc62d6a6f40c88ca261a9ebf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d53fc484eb8c2416cd0bb535d6f31dc.js"
+  "/precache-manifest.e1e64b9ac9b3de04452d1bc761a5a8ea.js"
 );
 
 workbox.clientsClaim();
